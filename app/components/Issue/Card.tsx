ea08a87767414c75f8c2c932bf3934e472083923
Seokmin Lee <essemfly@gmail.com>
2023-08-17 09:30:02 +0900

    Add dayjs and fix gradation

--- app/components/Issue/Card.tsx
+++ app/components/Issue/Card.tsx
@@ -1,12 +1,15 @@
 import { memo } from 'react';
 import Link from 'next/link';
-import { IssueWithCelebs } from '@/models/issue.server';
-
-import ImageFallback from '@/components/ImageFallback/ImageFallback';
-import CelebGroup from '@/components/AvatarGroup/CelebGroup';
+import { format, register } from 'timeago.js';
+import koLocale from 'timeago.js/lib/lang/ko';
 import { Card as AntdCard } from 'antd';
 
+import { IssueWithCelebs } from '@/models/issue.server';
+import ImageFallback from '@/components/ImageFallback/ImageFallback';
+import CelebGroup from '@/components/AvatarGroup/CelebGroup';
+
 const { Meta } = AntdCard;
+register('ko', koLocale);
 
 type CardProps = IssueWithCelebs;
 
@@ -30,7 +33,7 @@ const Card = ({
             style={{ border: '1px solid #DFDFDF' }}
             hoverable
             cover={
-              <>
+              <div style={{ position: 'relative' }}>
                 <ImageFallback
                   className="h-64 w-full object-cover"
                   alt={title}
@@ -49,13 +52,33 @@ const Card = ({
                   // fill
                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 />
+                <div
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    zIndex: 5,
+                    background:
+                      'linear-gradient(180deg, rgba(0, 0, 0, 0.4) 21.35%, rgba(0, 0, 0, 0) 34.9%)',
+                  }}
+                ></div>
                 <span
-                  className="text-white p-1 absolute top-2 left-2 font-extralight text-sm"
-                  style={{ position: 'absolute', top: 0 }}
+                  className="text-white ㅗp-1 absolute top-2 left-2 font-extralight text-sm"
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    color: 'white',
+                    fontSize: '14px',
+                    letterSpacing: '-0.005em',
+                    margin: '8px',
+                    zIndex: 10,
+                  }}
                 >
-                  {JSON.parse(JSON.stringify(updatedAt))}
+                  {format(updatedAt, 'ko')} 업데이트
                 </span>
-              </>
+              </div>
             }
           >
             <Meta title={title} description={description} />
--- app/components/Issue/Intro.tsx
+++ app/components/Issue/Intro.tsx
@@ -1,13 +1,24 @@
+import { format, register } from 'timeago.js';
+import koLocale from 'timeago.js/lib/lang/ko';
+
+register('ko', koLocale);
+
 type IntroProps = { title: string; description: string; updatedAt: string };
 
 const Intro = ({ title, updatedAt }: IntroProps) => {
   return (
     <div className="space-y-4 px-4 py-8 text-center md:px-12">
-      <h1 className="PlaygroundEditorTheme__h1">
-        {title}
-      </h1>
-      <div className="text-left text-sm text-gray-400" style={{margin: "12px 0"}}>
-        마지막 업데이트: {updatedAt}
+      <h1 className="PlaygroundEditorTheme__h1">{title}</h1>
+      <div
+        className="text-left text-sm text-gray-400"
+        style={{
+          margin: '12px 0',
+          fontSize: '15px',
+          fontWeight: 400,
+          color: '#89898D',
+        }}
+      >
+        마지막 업데이트: {format(updatedAt, 'ko')}
       </div>
     </div>
   );
