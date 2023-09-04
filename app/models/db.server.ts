01f016d000490d3b5231396ce51b8077a9699b19
Seokmin Lee <essemfly@gmail.com>
2023-09-04 15:04:05 +0900

    Remove getting database url code

--- app/models/db.server.ts
+++ app/models/db.server.ts
@@ -11,35 +11,12 @@ declare global {
 // create a new connection to the DB with every change either.
 // in production we'll have a single connection to the DB.
 if (process.env.NODE_ENV === 'production') {
-  prisma = getClient();
+  prisma = new PrismaClient();
 } else {
   if (!global.__db__) {
-    global.__db__ = getClient();
+    global.__db__ = new PrismaClient();
   }
   prisma = global.__db__;
-}
-
-function getClient() {
-  const { DATABASE_URL } = process.env;
-
-  const databaseUrl = new URL(DATABASE_URL || '');
-
-  console.log(`🔌 setting up prisma client to ${databaseUrl.host}`);
-  // NOTE: during development if you change anything in this function, remember
-  // that this only runs once per server restart and won't automatically be
-  // re-run per request like everything else is. So if you need to change
-  // something in this file, you'll need to manually restart the server.
-  const client = new PrismaClient({
-    datasources: {
-      db: {
-        url: databaseUrl.toString(),
-      },
-    },
-  });
-  // connect eagerly
-  client.$connect();
-
-  return client;
 }
 
 export { prisma };
